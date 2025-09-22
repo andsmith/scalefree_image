Fixed single image, circular units.

image_learn.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import tensorflow as tf
 import tempfile
@@ -266,17 +267,15 @@ class UIDisplay(object):
         self._output_shape = None  # for saving:  set during training, train_image.shape * display_multiplier, high res-generated image
         self._frame = None
 
-        self._interactive = not just_image
-        if just_image:
-            self._write_image()
-            self._shutdown = True
-            return
-        
+        self._just_image =  just_image
+
     def get_filename(self, which='model'):
         if which == 'model':
             return "%s_model_%s_%id_%ih.pkl" % (self._file_prefix, self._sim.div_type, self._sim.n_dividers, self._sim.n_hidden)
-        elif which == 'image':
+        elif which == 'frame':
             return "%s_%s_%id_%ih_cycle-%.8i.png" % (self._file_prefix, self._sim.div_type, self._sim.n_dividers, self._sim.n_hidden, self._sim.cycle)
+        elif which=='single-image':
+            return "%s_single_%s_%id_%ih.png" % (self._file_prefix, self._sim.div_type, self._sim.n_dividers, self._sim.n_hidden)
         else:
             raise Exception("Unknown filename type:  %s" % (which,))
 
@@ -307,7 +306,8 @@ class UIDisplay(object):
             if self._shutdown:
                 break
 
-            self._output_image = self._write_image() # Create & save image
+            self._output_image = self._gen_image()
+            self._write_frame(self._output_image) # Create & save image
 
             filename = self.get_filename('model')
             out_path = filename # Just write to cwd instead of os.path.join(img_dir, filename)
@@ -328,11 +328,20 @@ class UIDisplay(object):
             logging.info("  ffmpeg -y -framerate 10 -i %s_%s_%id_%ih_cycle-%%08d.png -c:v libx264 -pix_fmt yuv420p output_movie.mp4" %
                          (os.path.join(self._frame_dir, self._file_prefix), self.div_type, self.n_dividers, self.n_hidden))
 
-    def _write_image(self):
-        self._out_shape = (np.array(self._sim.image_train.shape[:2]) * self._display_multiplier).astype(int)
-        img = self._sim.gen_image(output_shape=self._out_shape, border=self._border)
+    def _gen_image(self, shape=None):
+        if shape is None:
+            shape = (np.array(self._sim.image_train.shape[:2]) * self._display_multiplier).astype(int)
+        img = self._sim.gen_image(output_shape=shape, border=self._border)
+        return img
+    
+    def _write_image(self, img):
+        out_filename = self.get_filename('single-image')
+        cv2.imwrite(out_filename, np.uint8(255 * img[:, :, ::-1]))
+        logging.info("Wrote:  %s" % (out_filename,))
+
+    def _write_frame(self, img):
         if self._frame_dir is not None:
-            out_filename = self.get_filename('image')
+            out_filename = self.get_filename('frame')
             out_path = os.path.join(self._frame_dir, out_filename)
             if os.path.exists(out_path):
                 logging.warning("Output image %s already exists, overwriting!!!!" % (out_path,))
@@ -360,8 +369,13 @@ class UIDisplay(object):
            * middle is the current output image
            * right is the loss history
         """
-        
-        self._start()
+        if self._just_image:
+            img_shape = (np.array(self._sim.image_raw.shape[:2]) * self._display_multiplier).astype(int)
+            img = self._gen_image(shape=img_shape)
+            self._write_image(img)
+            return
+                
+        self._start()  # start thread, training / eval cycle
 
         while self._sim.image_train is None:
             time.sleep(0.05)

make_movie.py
@@ -38,39 +38,39 @@ import re
 import numpy as np
 
 test_data_linear = {'frame_rate': 20,
-             'resolution': (0, 0), # use first image size
-             'caption_height_px': 50,  # shrink/grow if if you need more/fewer lines of text
-             'title_pause_sec': 5.0,
-             'episode_initial_pause_sec': 1.0,
-             'episode_final_pause_sec': 3.0,
-             'txt_color': (255, 255, 255),
-             'bkg_color': (0, 0, 0),
-             'title_pad_px': 30,
-             'caption_pad_xy': (10, 5),
-             'title': {'main': ('Cut the plane with N lines,',
-                                'color each region uniformly.',
-                                 'Reduce error by adjusting',
-                                 'lines to approximate target:',
-                                 '   F(x,y) = (R, G, B)'),
-                       'sub1': ('N:  8, 32, 128, 512          ',),
-                       'sub2': ('By: Andrew T. Smith, 2025      ',
-                                'github:andsmith/scalefree_image')
-                       },
-             'episodes': [
-                 {'input_pattern': r'movies\\washington_linear_8d_10h_cycle-????????.png',
-                  'caption': ['8 linear dividers', '10 hidden units']},
-                 {'input_pattern': r'movies\\washington_linear_32d_10h_cycle-????????.png',
-                  'caption': ['32 linear dividers', '10 hidden units']},
-                 {'input_pattern': r'movies\\washington_linear_128d_32h_cycle-????????.png',
-                  'caption': ['128 linear dividers', '32 hidden units']},
-                 {'input_pattern': r'movies\\washington_linear_512d_64h_cycle-????????.png',
-                  'caption': ['512 linear dividers', '64 hidden units']}
-             ]
-             }
+                    'resolution': (0, 0),  # use first image size
+                    'caption_height_px': 50,  # shrink/grow if if you need more/fewer lines of text
+                    'title_pause_sec': 5.0,
+                    'episode_initial_pause_sec': 1.0,
+                    'episode_final_pause_sec': 3.0,
+                    'txt_color': (255, 255, 255),
+                    'bkg_color': (0, 0, 0),
+                    'title_pad_px': 30,
+                    'caption_pad_xy': (10, 5),
+                    'title': {'main': ('Cut the plane with N lines,',
+                                       'color each region uniformly.',
+                                       'Reduce error by adjusting',
+                                       'lines to approximate target:',
+                                       '   F(x,y) = (R, G, B)'),
+                              'sub1': ('N:  8, 32, 128, 512          ',),
+                              'sub2': ('By: Andrew T. Smith, 2025      ',
+                                       'github:andsmith/scalefree_image')
+                              },
+                    'episodes': [
+                        {'input_pattern': r'movies\\washington_linear_8d_10h_cycle-????????.png',
+                         'caption': ['8 linear dividers', '10 hidden units']},
+                        {'input_pattern': r'movies\\washington_linear_32d_10h_cycle-????????.png',
+                         'caption': ['32 linear dividers', '10 hidden units']},
+                        {'input_pattern': r'movies\\washington_linear_128d_32h_cycle-????????.png',
+                         'caption': ['128 linear dividers', '32 hidden units']},
+                        {'input_pattern': r'movies\\washington_linear_512d_64h_cycle-????????.png',
+                         'caption': ['512 linear dividers', '64 hidden units']}
+                    ]
+                    }
 
 
 test_data = {'frame_rate': 20,
-             'resolution': (0, 0), # use first image size
+             'resolution': (0, 0),  # use first image size
              'caption_height_px': 50,  # shrink/grow if if you need more/fewer lines of text
              'title_pause_sec': 5.0,
              'episode_initial_pause_sec': 1.0,
@@ -79,24 +79,28 @@ test_data = {'frame_rate': 20,
              'bkg_color': (0, 0, 0),
              'title_pad_px': 30,
              'caption_pad_xy': (10, 5),
-             'title': {'main': ('Cut the plane with N lines,',
-                                'color each region uniformly.',
-                                 'Reduce error by adjusting',
-                                 'lines to approximate target:',
-                                 '   F(x,y) = (R, G, B)'),
+             'title': {'main': ('Cut the plane with N circles.',
+                                'Color each region uniformly.',
+                                'Pick circle size/position',
+                                'and color intersections to',
+                                'best approximate target.'),
                        'sub1': ('N:  8, 32, 128, 512          ',),
                        'sub2': ('By: Andrew T. Smith, 2025      ',
                                 'github:andsmith/scalefree_image')
                        },
              'episodes': [
                  {'input_pattern': r'movies\\mona_lisa_circular_8d_10h_cycle-????????.png',
-                  'caption': ['8 linear dividers', '10 hidden units']},
+                  'caption': ['8 circle units', '10 color units']},
                  {'input_pattern': r'movies\\mona_lisa_circular_32d_16h_cycle-????????.png',
-                  'caption': ['32 linear dividers', '10 hidden units']},
+                  'caption': ['32 circle units', '16 color units']},
+                 {'input_pattern': r'movies\\mona_lisa_circular_128d_24h_cycle-????????.png',
+                  'caption': ['128 circle units', '24 color units']},
                  {'input_pattern': r'movies\\mona_lisa_circular_512d_64h_cycle-????????.png',
-                  'caption': ['512 linear dividers', '64 hidden units']}
+                  'caption': ['512 circle units', '64 color units']}
              ]
              }
+
+
 class MovieMaker(object):
     def __init__(self, movie_json, output_file, preview=False):
         self.movie_json = movie_json
@@ -118,7 +122,7 @@ class MovieMaker(object):
         self.title_dur_sec = self.movie_data['title_pause_sec']
         self.txt_color = tuple(self.movie_data['txt_color'])
         self.bkg_color = tuple(self.movie_data['bkg_color'])
-        #self.episodes = self._load()
+        # self.episodes = self._load()
 
     def _make_title_frame(self, size_wh, spacing_frac=0.0):
         """
@@ -143,7 +147,8 @@ class MovieMaker(object):
         text_width = size_wh[0]-2*pad_px
 
         # Determine relative heights of each text box
-        n_lines = [len(self.title_txt['main']) + 1, len(self.title_txt['sub1']), len(self.title_txt['sub2'])] # +1 for main title sizing
+        n_lines = [len(self.title_txt['main']) + 1, len(self.title_txt['sub1']),
+                   len(self.title_txt['sub2'])]  # +1 for main title sizing
         total_lines = sum(n_lines)
         if total_lines == 0:
             total_lines = 1
@@ -152,23 +157,26 @@ class MovieMaker(object):
         rel_heights = [rh * (text_height - pad_height * (len(n_lines) - 1)) for rh in rel_heights]
         box_heights = [int(rh) for rh in rel_heights]
 
-        main_bbox = {'x':(pad_px, pad_px+text_width),
-                     'y':(pad_px, pad_px + box_heights[0])}
+        main_bbox = {'x': (pad_px, pad_px+text_width),
+                     'y': (pad_px, pad_px + box_heights[0])}
         y_top = pad_px + box_heights[0] + pad_height
-        sub1_bbox = {'x':(pad_px, pad_px+text_width),
-                     'y':(y_top, y_top + box_heights[1])}
+        sub1_bbox = {'x': (pad_px, pad_px+text_width),
+                     'y': (y_top, y_top + box_heights[1])}
         y_top = y_top + box_heights[1] + pad_height
-        sub2_bbox = {'x':(pad_px, pad_px+text_width),
-                     'y':(y_top, y_top + box_heights[2])}
-        
+        sub2_bbox = {'x': (pad_px, pad_px+text_width),
+                     'y': (y_top, y_top + box_heights[2])}
+
         frame = np.zeros((size_wh[1], size_wh[0], 3), dtype=np.uint8)
         frame[:, :] = self.bkg_color
-        
-        add_text(frame, self.title_txt['main'], main_bbox,font_face = cv2.FONT_HERSHEY_COMPLEX, justify='center',color=self.txt_color,line_spacing=2.5,)
-        add_text(frame, self.title_txt['sub1'], sub1_bbox, font_face = cv2.FONT_HERSHEY_SIMPLEX, justify='center', color=self.txt_color)
-        add_text(frame, self.title_txt['sub2'], sub2_bbox, font_face = cv2.FONT_HERSHEY_SIMPLEX, justify='left', line_spacing=2.5, color=self.txt_color)
+
+        add_text(frame, self.title_txt['main'], main_bbox, font_face=cv2.FONT_HERSHEY_COMPLEX,
+                 justify='center', color=self.txt_color, line_spacing=2.5,)
+        add_text(frame, self.title_txt['sub1'], sub1_bbox,
+                 font_face=cv2.FONT_HERSHEY_SIMPLEX, justify='center', color=self.txt_color)
+        add_text(frame, self.title_txt['sub2'], sub2_bbox, font_face=cv2.FONT_HERSHEY_SIMPLEX,
+                 justify='left', line_spacing=2.5, color=self.txt_color)
         return frame
-    
+
     def run(self):
         frames = self._make_frame_sequence()
         if self.preview:
@@ -180,6 +188,7 @@ class MovieMaker(object):
                 if (f.shape[1], f.shape[0]) != frame_size:
                     raise ValueError("All frames must have the same size to write movie, but found differing sizes.")
             self._write_movie(frames)
+
     def _preview(self, frames):
         for i, frame in enumerate(frames):
             cv2.imshow('preview', frame)
@@ -187,6 +196,7 @@ class MovieMaker(object):
             if key == 27 or key == ord('q'):
                 break
         cv2.destroyAllWindows()
+
     def _load_episode_frames(self, episode):
         files = self._get_files(episode['input_pattern'])
         if not files:
@@ -203,7 +213,8 @@ class MovieMaker(object):
                     img = cv2.resize(img, self.resolution, interpolation=cv2.INTER_AREA)
             else:
                 if frames and (img.shape[1] != frames[0].shape[1] or img.shape[0] != frames[0].shape[0]):
-                    logging.warning(f"Image {f} has different size ({img.shape[1]}, {img.shape[0]}) than previous images ({frames[0].shape[1]}, {frames[0].shape[0]}).")
+                    logging.warning(
+                        f"Image {f} has different size ({img.shape[1]}, {img.shape[0]}) than previous images ({frames[0].shape[1]}, {frames[0].shape[0]}).")
             frames.append(img)
         if not frames:
             raise ValueError(f"No valid image files found for episode with pattern:  {episode['input_pattern']}")
@@ -213,53 +224,55 @@ class MovieMaker(object):
     def _make_episode_seq(self, episode):
         frames = self._load_episode_frames(episode)
         frame_captions = episode['caption']
-        frames = [captioned_frame(f, frame_captions, self.caption_height_px, self.caption_pad_xy, txt_color=self.txt_color, bkg_color=self.bkg_color) for f in frames]
+        frames = [captioned_frame(f, frame_captions, self.caption_height_px, self.caption_pad_xy,
+                                  txt_color=self.txt_color, bkg_color=self.bkg_color) for f in frames]
         intro_frame = frames[0]
         outro_frame = frames[-1]
-        frames = self._mk_seq(intro_frame, self.initial_pause_sec) + frames + self._mk_seq(outro_frame, self.final_pause_sec)
-        
+        frames = self._mk_seq(intro_frame, self.initial_pause_sec) + frames + \
+            self._mk_seq(outro_frame, self.final_pause_sec)
+
         return frames
 
     def _make_frame_sequence(self):
         episode_sequences = [self._make_episode_seq(ep) for ep in self.episode_data]
         frame_size_wh = episode_sequences[0][0].shape[:2][::-1]
         title_frame = self._make_title_frame(frame_size_wh)
-        
+
         frames = self._mk_seq(title_frame, self.title_dur_sec)
         for seq in episode_sequences:
             frames += seq
         return frames
-    
+
     def _mk_seq(self, frame, dur_sec):
         n_frames = int(dur_sec * self.frame_rate)
         return [frame] * n_frames
-    
+
     def _write_movie(self, frames):
         if not frames:
             logging.error("No frames to write")
             return
-            
+
         # Get frame dimensions from first frame and ensure they're consistent
         first_frame = frames[0]
         height, width, channels = first_frame.shape
-        
+
         # Ensure all frames have the same dimensions
         for i, frame in enumerate(frames):
             if frame.shape != first_frame.shape:
                 logging.warning(f"Frame {i} has different shape {frame.shape}, resizing to {first_frame.shape}")
                 frames[i] = cv2.resize(frame, (width, height))
-        
+
         # Make sure frame dimensions are even (required for yuv420p)
         if width % 2 != 0:
             width -= 1
         if height % 2 != 0:
             height -= 1
-            
+
         # Resize frames to even dimensions if needed
         if width != first_frame.shape[1] or height != first_frame.shape[0]:
             logging.info(f"Adjusting frame size to {width}x{height} for yuv420p compatibility")
             frames = [cv2.resize(frame, (width, height)) for frame in frames]
-        
+
         cmd = ['ffmpeg',
                '-y',  # overwrite output file if it exists
                '-f', 'rawvideo',  # specify input format
@@ -273,25 +286,25 @@ class MovieMaker(object):
                '-r', str(self.frame_rate),  # output framerate
                '-preset', 'medium',  # encoding preset
                self.output_file]
-        
+
         logging.info("Running ffmpeg command:  %s" % (' '.join(cmd),))
         logging.info(f"Frame dimensions: {width}x{height}, channels: {channels}")
-        
+
         try:
             proc = subprocess.Popen(cmd, stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-            
+
             for i, frame in enumerate(frames):
                 if proc.poll() is not None:  # Check if process has terminated
                     logging.error(f"FFmpeg process terminated early at frame {i}")
                     break
-                    
+
                 if i % 100 == 0:  # Log progress every 100 frames
                     logging.info(f"Writing frame {i}/{len(frames)}")
-                    
+
                 # Ensure frame is contiguous in memory
                 if not frame.flags['C_CONTIGUOUS']:
                     frame = np.ascontiguousarray(frame)
-                    
+
                 try:
                     frame_bytes = frame.tobytes()
                     expected_size = width * height * channels
@@ -302,17 +315,17 @@ class MovieMaker(object):
                 except BrokenPipeError:
                     logging.error(f"Broken pipe error at frame {i}")
                     break
-                    
+
             proc.stdin.close()
             stdout, stderr = proc.communicate()
-            
+
             if proc.returncode != 0:
                 logging.error(f"ffmpeg exited with code {proc.returncode}")
                 if stderr:
                     logging.error(f"ffmpeg stderr: {stderr.decode()}")
             else:
                 logging.info(f"Movie written to {self.output_file}")
-                
+
         except Exception as e:
             logging.error(f"Error during movie creation: {e}")
             if 'proc' in locals():
@@ -330,17 +343,18 @@ class MovieMaker(object):
         digit_count = glob_pattern.count('?')
         regex_pattern = re.escape(glob_pattern).replace(r'\?' * digit_count, r'(\d{' + str(digit_count) + r'})')
         regex = re.compile(regex_pattern)
-        
+
         def extract_number(f):
             match = regex.search(f)
             if match:
                 return int(match.group(1))
             else:
                 return float('inf')  # If no match, put it at the end
-        
+
         files.sort(key=extract_number)
-        return files    
-    
+        return files
+
+
 def get_args():
     parser = argparse.ArgumentParser(description="Make a movie from frames")
     parser.add_argument('movie_json', type=str, help="Json file describing the movie")
@@ -350,12 +364,13 @@ def get_args():
 
     return parser.parse_args()
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     args = get_args()
     if args.example:
         example = deepcopy(test_data)
-        
+
         with open('example_movie.json', 'w') as f:
             json.dump(example, f, indent=4)
         logging.info("Wrote example_movie.json")
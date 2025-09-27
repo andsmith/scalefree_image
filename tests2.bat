df4a8b3ef6938a6e2c27b6cabf05f52e9a11fd3f
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2025-09-26 23:23:01 -0700

    update tests for 3param

--- tests2.bat
+++ tests2.bat
@@ -1,5 +1,7 @@
-python .\image_learn.py -i .\input\test_3_lines.png -l 4  -t 10 -n 10 -p 15 -x 8 -e 100  -k 300  -r 1.0 -a .001 -z 8 --gradient_sharpness 1.0 --save_frames test_4_lines_2_param_gs1_anneal --nogui
-python .\image_learn.py -i .\input\test_3_lines.png -l 4  -t 10 -n 10 -p 15 -x 8 -e 100  -k 300  -r 1.0 -a .001 -z 8 --gradient_sharpness 2.0 --save_frames test_4_lines_2_param_gs2_anneal --nogui
-python .\image_learn.py -i .\input\test_3_lines.png -l 4  -t 10 -n 10 -p 15 -x 8 -e 100  -k 300  -r 1.0 -a .001 -z 8 --gradient_sharpness 5.0 --save_frames test_4_lines_2_param_gs5_anneal --nogui
-python .\image_learn.py -i .\input\test_3_lines.png -l 4  -t 10 -n 10 -p 15 -x 8 -e 100  -k 300  -r 1.0 -a .001 -z 8 --gradient_sharpness 10.0 --save_frames test_4_lines_2_param_gs10_anneal --nogui
+python .\image_learn.py -i .\input\test_3_lines.png -l 4  -t 10 -n 10 -p 15 -x 8 -e 100  -k 300  -r 1.0 -a .001 -z 8 --gradient_sharpness 1.0 --save_frames test_4_lines_3_param_gs1_anneal --nogui
+python .\image_learn.py -i .\input\test_3_lines.png -l 4  -t 10 -n 10 -p 15 -x 8 -e 100  -k 300  -r 1.0 -a .001 -z 8 --gradient_sharpness 2.0 --save_frames test_4_lines_3_param_gs2_anneal --nogui
+python .\image_learn.py -i .\input\test_3_lines.png -l 4  -t 10 -n 10 -p 15 -x 8 -e 100  -k 300  -r 1.0 -a .001 -z 8 --gradient_sharpness 5.0 --save_frames test_4_lines_3_param_gs5_anneal --nogui
+python .\image_learn.py -i .\input\test_3_lines.png -l 4  -t 10 -n 10 -p 15 -x 8 -e 100  -k 300  -r 1.0 -a .001 -z 8 --gradient_sharpness 10.0 --save_frames test_4_lines_3_param_gs10_anneal --nogui
+
+
 

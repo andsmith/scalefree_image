b55036a3ef550c428df365c709e3036a8316c410
Andrew T. Smith <andrew.tombras.smith@gmail.com>
2025-10-05 18:45:46 -0700

    Added clobber, nogui, run_cycles=n options to nnet_test.py

--- nnet_test.py
+++ nnet_test.py
@@ -22,8 +22,8 @@ from copy import deepcopy
 import os
 
 class ScaleFreeImage(object):
-    def __init__(self, image_filename, out_dir, layers=(100,100), batch_size=1024, n_train_sample=16384,
-                    display_multiplier=1.0, learning_rate=0.001, weights_file=None, epochs_per_cycle=100):
+    def __init__(self, image_filename, out_dir, layers=(100,100), batch_size=1024, n_train_sample=16384, cycles_to_run=-1,
+                    display_multiplier=1.0, learning_rate=0.001, weights_file=None, epochs_per_cycle=100, clobber=False):
         self.learning_rate = learning_rate
         self._shutdown = False
         self._disp_mul = display_multiplier
@@ -33,6 +33,7 @@ class ScaleFreeImage(object):
         self.out_dir= out_dir
         self._win_name = "Feed-forward network %s learning scale-free image." % (str(layers),)
         self._filename_base =self._get_base_filename(image_filename)
+        self._clobber=clobber
         self._epoch_over = Event()
         if not os.path.exists(self.out_dir):
             os.makedirs(self.out_dir)
@@ -47,7 +48,7 @@ class ScaleFreeImage(object):
             self.ylim = -1.0, 1.0
             self.xlim = -1.0 * self.aspect, 1.0 * self.aspect
         
-        
+        self.cycles_to_run = cycles_to_run
         self.cycle_ind = 0
         self.epoch_ind=0
         self.epochs_per_cycle = epochs_per_cycle
@@ -59,17 +60,20 @@ class ScaleFreeImage(object):
 
         logging.info("Input shape: %s" % (self.train_image.shape,))
         logging.info("Output shape: %s" % (self.output_image.shape,))
-        cv2.namedWindow(self._win_name, cv2.WINDOW_NORMAL)
-
+        
         self._history = [] # one entry per update cycle, keys "learning_rate" (float), "losses" (1 list per epoch of minibatch losses (floats))
 
         if weights_file is None:
-            weights_file = self._get_filename('weights')
-            if not os.path.exists(weights_file):
+            if self._clobber:
                 weights_file = None
-                logging.info("No existing weights file found at %s. Starting new model." % (weights_file,))
+                logging.info("Clobber is True, starting new model.")
             else:
-                logging.info("Resuming training from existing weights file: %s" % (weights_file,))
+                weights_file = self._get_filename('weights')
+                if not os.path.exists(weights_file):
+                    weights_file = None
+                    logging.info("No existing weights file found at %s. Starting new model." % (weights_file,))
+                else:
+                    logging.info("Resuming training from existing weights file: %s" % (weights_file,))
 
         self._init_model(weights_file)
         
@@ -113,7 +117,7 @@ class ScaleFreeImage(object):
         if which == 'weights':
             fname="%s.model_weights.npz" % (file_base,)
         elif which == 'frame':
-            fname="%s_frame_%08d.png" % (file_base, self.cycle_ind)
+            fname="%s_frame_%08d.png" % (file_base, self.cycle_ind+1)
         elif which == 'metadata':
             fname="%s.metadata.json" % (file_base,)
         elif which == 'train_image':
@@ -181,13 +185,17 @@ class ScaleFreeImage(object):
         self._model = Model(inputs=inputs, outputs=outputs)
         optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
         
-        if weights_file is not None:
-            weights = np.load(weights_file)
-            wts = [weights[f'arr_{i}'] for i in range(len(weights.files))]
-            self._model.set_weights(wts)
-            logging.info(f"Loaded model weights from {weights_file}")
-            
-            self._read_metadata()
+        if not self._clobber:
+            if weights_file is not None:
+                weights = np.load(weights_file)
+                wts = [weights[f'arr_{i}'] for i in range(len(weights.files))]
+                self._model.set_weights(wts)
+                logging.info(f"Loaded model weights from {weights_file}")
+                
+                self._read_metadata()
+        else:
+            logging.info("Clobber is True, starting new model.")
+        
             
         self._model.compile(optimizer=optimizer, loss='mse')
         
@@ -231,12 +239,23 @@ class ScaleFreeImage(object):
             b = ndimage.map_coordinates(self.train_image[:,:,2], [y_pixel.flatten(), x_pixel.flatten()], order=1)
             self._output = np.vstack((r,g,b)).T
 
-    def _train_loop(self,verbose=False,n_cycles=-1):
+    def _train_loop(self,verbose=False):
         n_new_cycles = 0
+        output_pred = self._model.predict(self._output_xy, verbose=verbose, batch_size=65536)
+        self.output_image = self._output_vec_to_image(output_pred, self.output_shape)
+
+        if self.cycle_ind==0:
+            # We're starting fresh, create an initial output image:
+            self.cycle_ind = -1  # so it is frame 00000000.png
+            frame_name = self._get_filename('frame')
+            self.cycle_ind = 0
+            cv2.imwrite(frame_name, self.output_image)
+            
+        cycle_times = []
         while not self._shutdown:  # Loop over update cycles:
             logging.info("Starting training cycle %d (%i this run), training on %i samples for %i epochs with batch size %i at learning rate %.6f"%(
                 self.cycle_ind, n_new_cycles, self.n_train, self.epochs_per_cycle, self.batch_size, self.learning_rate))
-            if n_cycles > -1 and n_new_cycles >= n_cycles:
+            if self.cycles_to_run > -1 and n_new_cycles >= self.cycles_to_run:
                 break
             
             # TRAIN ONE CYCLE:
@@ -253,24 +272,36 @@ class ScaleFreeImage(object):
                                 batch_size=self.batch_size,
                                 verbose=verbose, 
                                 callbacks=[loss_tracker])
-                print("\tEpoch %d loss: %.6f"%(epoch, loss.history['loss'][0]))
+                logging.info("\t\tEpoch %d loss: %.6f"%(epoch, loss.history['loss'][0]))
             cycle_info= {'losses': loss_tracker.losses,'learning_rate': self.learning_rate}    
-            
             # RECOMPUTE OUTPUT IMAGE:
 
             output_pred = self._model.predict(self._output_xy, verbose=verbose, batch_size=32768)
-            output_image = self._output_vec_to_image(output_pred, self.output_shape)
             # SAVE STATE:
             end_time = time.time()
             cycle_time = end_time - start_time
-            #logging.info(f"Completed training cycle {self.cycle_ind} in {cycle_time:.2f} seconds.")
-            
+            cycle_times.append(cycle_time)
+
+            logging.info(f"\tCompleted training cycle {self.cycle_ind} in {cycle_time:.2f} seconds.")
+
             frame_name = self._get_filename('frame')
+            output_image = self._output_vec_to_image(output_pred, self.output_shape)
             cv2.imwrite(frame_name, output_image)
-            #logging.info(f"Saved output image to {frame_name}")
+            
+            logging.info(f"\tSaved output image to {frame_name}")
             cycle_info['cycle_time'] = cycle_time
             cycle_info['frame_file'] = frame_name
             
+            if self.cycles_to_run > 0:
+                mean_cyc_t = np.mean(cycle_times[-5:])  # mean of last 5 cycles
+                cycle_time_d = mean_cyc_t if not np.isnan(mean_cyc_t) else cycle_time
+                n_cycles_left = self.cycles_to_run - n_new_cycles - 1
+                eta_sec = n_cycles_left * cycle_time_d
+                eta_hr, eta_min, eta_sec = int(eta_sec/3600.0), int((eta_sec%3600)/60.0), int(eta_sec%60.0)
+                logging.info(f"\t\tLast cycle completed in {cycle_time:.2f} seconds.")
+                logging.info(f"\t\tMean of past 5 cycles: {int(mean_cyc_t/60.0)}:{(mean_cyc_t%60.0):.2f} (MM:SS)")
+                logging.info(f"\t\tETA for {n_cycles_left} cycles: {eta_hr}:{eta_min}:{eta_sec} (HH:MM:SS)")
+
             with self._update_lock:
                 self._history.append(cycle_info)
                 self.output_image = output_image
@@ -280,22 +311,26 @@ class ScaleFreeImage(object):
             n_new_cycles += 1
             
         logging.info("Training thread exiting.")
+        self._shutdown =True # Stop the GUI if we ran out of cycles.
         
         
-    def run(self, debug_mode=False):
+    def run(self, debug_mode=False,no_gui =False):
         """"""
         if debug_mode:
-            self._train_loop(n_cycles=1)  # Debug, no thread
+            self._train_loop()  # Debug, no thread
             # Save training image
         else:
             train_thread = Thread(target=self._train_loop)
             train_thread.start()    
         
-        # GUI loop
+        if no_gui:
+            logging.info("Running in no-GUI mode, waiting for training to complete.")
+            return  
         
-        
-        ""
-        
+        cv2.namedWindow(self._win_name, cv2.WINDOW_NORMAL)
+        out_w, out_h = self.output_shape[1], self.output_shape[0]
+        cv2.resizeWindow(self._win_name, out_w, out_h)
+
         artists = {'image': None,
                    'train_xy': None,
                    'learning_rate' : None, 
@@ -347,11 +382,13 @@ def get_args():
     parser.add_argument('-l', '--layers', type=int, nargs='+', default=[100, 100], help='List of hidden layer sizes.')
     parser.add_argument('-b', '--batch_size', type=int, default=1024, help='Batch size for training.')
     parser.add_argument('-t', '--num_train', type=int, default=16384, help='Number of x,y positions to sample for each epoch (or 0 to just use pixel positions).')
-    
+    parser.add_argument('-k', '--num_cycles', type=int, default=-1, help='Number of training cycles to run before exiting (-1 to run forever).')
     parser.add_argument('-e', '--epochs_per_update', type=int, default=100, help='Number of epochs to train per update cycle.')
     parser.add_argument('-x', '--display_multiplier', type=float, default=1.0, help='The output frame will be this X the size of the training image.')
     parser.add_argument('-r', '--learning_rate', type=float, default=0.001, help='Learning rate for the optimizer.')
     parser.add_argument('-w', '--weights_file', type=str, default=None, help='Path to load model weights from.')
+    parser.add_argument('--nogui', action='store_true', help='Run in no-GUI mode (just training).')
+    parser.add_argument('--clobber', action='store_true', help='Overwrite existing files, train from scratch etc.')
     
 
     args = parser.parse_args()
@@ -366,11 +403,13 @@ def app():
                          layers=args.layers, 
                          batch_size=args.batch_size, 
                          epochs_per_cycle=args.epochs_per_update,
+                         cycles_to_run=args.num_cycles,
                          display_multiplier=args.display_multiplier,
                          learning_rate=args.learning_rate,
                          weights_file=args.weights_file,
-                         n_train_sample=args.num_train)
-    sfi.run()
+                         n_train_sample=args.num_train,
+                         clobber=args.clobber)
+    sfi.run(no_gui = args.nogui)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
